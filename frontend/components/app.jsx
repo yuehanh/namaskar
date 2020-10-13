a3ec8c915a202db23f26f905774efca3fd87e88c
Yuehan <yuehan.huang@protonmail.com>
2020-10-13 14:51:36 -0700

    Reroute root to home when logged in

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Route, Switch, Link, Redirect } from "react-router-dom";
 
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+import { SplashContainer } from './splash/splash_container';
+import { HomeContainer } from './home/home_container';
 import { SignupFormContainer } from './session_form/signup_form_container';
 import { LoginFormContainer } from './session_form/login_form_container';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 export const App = () => (
   <div>
-    <h1>Namaskar</h1>
     <Switch>
+      <AuthRoute exact path="/" component={SplashContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <ProtectedRoute exact path="/home" component={HomeContainer} />
       <Redirect to="/" />
     </Switch>
   </div>
