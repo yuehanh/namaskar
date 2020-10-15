3f925cf42e5b26cb6bd4f21b758b7086e3592b7e
Yuehan <yuehan.huang@protonmail.com>
2020-10-14 17:37:36 -0700

    Style log out button on home

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom';
 
 export class Home extends React.Component {
   render() {
-    return (<div className='home-nav'>
-      <button onClick={this.props.logout} className="logout-button">Log Out</button>
-    </div>)
-
+    return (
+      <div className='splash'>
+        <div className="splash-nav">
+          <nav className='wrapper'>
+            <div className='nav-icons'>
+              <Link to='/'>
+                <img className='nav-logo' src={window.images.fullLogo} />
+              </Link>
+            </div>
+            <div className='nav-session'>
+              <button onClick={this.props.logout} className="sign-up nav-button">Log Out</button>
+            </div>
+          </nav>
+        </div>
+      </div>)
   }
 }
