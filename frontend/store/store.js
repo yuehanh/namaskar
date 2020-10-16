931d5aabf87af2d3616975c8b6f54c1ddeab8df4
Yuehan <yuehan.huang@protonmail.com>
2020-10-15 18:50:16 -0700

    Fix typoe redux-logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { rootReducer } from "../reducers/root_reducer"
-import logger from "reduc-logger"
+import logger from "redux-logger"
 import thunk from "redux-thunk";
 
 export const configureStore = (preloadedState = {}) =>
