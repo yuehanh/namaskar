c9dfdd73da51e1c13ab5062013e432aecad4cfed
Yuehan <yuehan.huang@protonmail.com>
2020-10-16 09:10:40 -0700

    Modify users reducer to fit the updated normalized state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import { usersReducer } from "./users_reducer";
+import { workspacesReducer } from "./workspaces_reducer";
+import { userWorkspacesReducer } from "./user_workspaces_reducer";
 
 export const entitiesReducer = combineReducers({
   users: usersReducer,
+  workspaces: workspacesReducer,
+  userWorkspaces: userWorkspacesReducer,
 });
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -5,7 +5,7 @@ export const usersReducer = (state = {}, action) => {
   let nextState = Object.assign({}, state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      nextState[action.payload.id] = action.payload;
+      nextState = action.payload.users;
       return nextState;
     default:
       return state;
