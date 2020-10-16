924cf24b2d0ae10e69a8743ce3523828c46620af
Yuehan <yuehan.huang@protonmail.com>
2020-10-16 15:19:58 -0700

    Remove destroy all in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.destroy_all
+# UserWorkspace.destroy_all
+# Workspace.destroy_all
+# User.destroy_all
 
 10.times do
   name = Faker::Name.unique.first_name
