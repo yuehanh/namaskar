7f4107b9763f5819d550f9c01bc02cf4e9dbbffb
Yuehan <yuehan.huang@protonmail.com>
2020-10-20 10:11:16 -0700

    Allow clicking anywhere  to close the menu

--- frontend/components/home/user_controller.jsx
+++ frontend/components/home/user_controller.jsx
@@ -11,40 +11,59 @@ export class UserController extends React.Component {
   }
 
   toggleMenu() {
-    this.setState({ menu: !this.state.menu })
+    if (this.state.menu) {
+      this.setState({ menu: !this.state.menu },
+        () => document.removeEventListener('click', this.toggleMenu))
+    }
+    else {
+      this.setState({ menu: !this.state.menu },
+        () => document.addEventListener('click', this.toggleMenu))
+    }
   }
 
+
   changeHomespace(workspaceId) {
-    this.props.updateUser({ id: this.props.currentUser.id, homespace_id: workspaceId })
+    this.props.updateUser({
+      id: this.props.currentUser.id,
+      homespace_id: workspaceId
+    })
   }
 
   render() {
+    const avatar = this.props.currentUser.fullname.slice(0, 2)
+    const homespace = <li key={this.props.homespace.id}
+      className="selected">
+      {this.props.homespace.name}
+    </li>
+
+    const workspaces = this.props.workspaces.map(workspace => {
+      if (this.props.homespace.id !== workspace.id) {
+        return (
+          <li key={workspace.id}
+            onClick={() => this.changeHomespace(workspace.id)}
+          >
+            { workspace.name}
+          </li>
+        )
+      }
+    })
+
     return (
       <div className="workspace-user-control" >
         <div
           className="avatar clickable"
-          onClick={this.toggleMenu}
-        >
-          {this.props.currentUser.fullname.slice(0, 2)}
+          onClick={this.toggleMenu}>
+          {avatar}
         </div>
-        <div className={`user-control-menu ${this.state.menu ? "" : "hidden"}`}>
-          <ul className="workspace-menu clickable" onClick={this.toggleMenu}>
-            <li key={this.props.homespace.id}
-              className="selected">{this.props.homespace.name}</li>
-            {this.props.workspaces.map(workspace => {
-              if (this.props.homespace.id !== workspace.id) {
-                return (
-                  <li key={workspace.id}
-                    onClick={() => this.changeHomespace(workspace.id)}
-                  >
-                    { workspace.name}
-                  </li>
-                )
-              }
 
-            })}
+        <div className={`user-control-menu ${this.state.menu ? "" : "hidden"}`}>
+          <ul className="workspace-menu clickable" >
+            {homespace}
+            {workspaces}
             <div className="menu-separator"></div>
-            <li><Link to="/workspaces/new">Create New Workspace</Link></li>
+            <li>
+              <Link to="/workspaces/new">Create New Workspace</Link>
+            </li>
             <li onClick={this.props.logout}>Log Out</li>
           </ul>
         </div>
