e4b75b98149c755d171bd47343fd9460253e9c77
Yuehan <yuehan.huang@protonmail.com>
2020-10-20 12:39:28 -0700

    Create project actions

---
+++ frontend/actions/project_actions.js
@@ -0,0 +1,49 @@
+import * as ProjectUtil from "../util/project_api_util";
+import { receiveError } from "./session_actions";
+
+export const RECEIVE_PROJECT = "RECEIVE_PROJECT";
+export const DELETE_PROJECT = "DELETE_PROJECT";
+
+const receiveProject = (payload) => {
+  return {
+    type: RECEIVE_PROJECT,
+    payload,
+  };
+};
+
+const destroyProject = (payload) => {
+  return {
+    type: DELETE_PROJECT,
+    payload,
+  };
+};
+
+export const createProject = (workspaceId, project) => (dispatch) => {
+  return ProjectUtil.createProject(workspaceId, project)
+    .then((payload) => dispatch(receiveProject(payload)))
+    .fail((payload) => {
+      dispatch(receiveError(payload.responseJSON));
+    });
+};
+export const updateProject = (project) => (dispatch) => {
+  return ProjectUtil.updateProject(project)
+    .then((payload) => dispatch(receiveProject(payload)))
+    .fail((payload) => {
+      dispatch(receiveError(payload.responseJSON));
+    });
+};
+export const fetchProject = (projectId) => (dispatch) => {
+  return ProjectUtil.fetchProject(projectId)
+    .then((payload) => dispatch(receiveProject(payload)))
+    .fail((payload) => {
+      return dispatch(receiveError(payload.responseJSON));
+    });
+};
+
+export const deleteProject = (projectId) => (dispatch) => {
+  return ProjectUtil.deleteProject(projectId)
+    .then(() => dispatch(destroyProject(projectId)))
+    .fail((payload) => {
+      dispatch(receiveError(payload.responseJSON));
+    });
+};
