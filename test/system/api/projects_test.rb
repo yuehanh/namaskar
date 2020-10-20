3a3aa11245ccba2f3cfe3ae570c0f2cef22bc7f8
Yuehan <yuehan.huang@protonmail.com>
2020-10-20 00:10:56 -0700

    Create project scaffold

---
+++ test/system/api/projects_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class ProjectsTest < ApplicationSystemTestCase
+  setup do
+    @project = projects(:one)
+  end
+
+  test "visiting the index" do
+    visit projects_url
+    assert_selector "h1", text: "Api/Projects"
+  end
+
+  test "creating a Project" do
+    visit projects_url
+    click_on "New Api/Project"
+
+    fill_in "Description", with: @project.description
+    fill_in "Lead", with: @project.lead_id
+    fill_in "Name", with: @project.name
+    fill_in "Workspace", with: @project.workspace_id
+    click_on "Create Project"
+
+    assert_text "Project was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Project" do
+    visit projects_url
+    click_on "Edit", match: :first
+
+    fill_in "Description", with: @project.description
+    fill_in "Lead", with: @project.lead_id
+    fill_in "Name", with: @project.name
+    fill_in "Workspace", with: @project.workspace_id
+    click_on "Update Project"
+
+    assert_text "Project was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Project" do
+    visit projects_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Project was successfully destroyed"
+  end
+end
