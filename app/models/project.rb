5fdf647c5176e40bcbfc4f87721c540c0b0859ad
Yuehan <yuehan.huang@protonmail.com>
2020-10-20 00:44:48 -0700

    Create project model

---
+++ app/models/project.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id           :bigint           not null, primary key
+#  name         :string           not null
+#  description  :string
+#  lead_id      :bigint           not null
+#  workspace_id :bigint
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Project < ApplicationRecord
+  validates :lead_id, :workspace_id, :name, presence: true
+
+  belongs_to :lead, class_name: :User
+  belongs_to :workspace
+end
