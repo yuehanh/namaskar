3ca3d4925cc6a48f6810316fa6fc3c78b8dc22c7
Yuehan <yuehan.huang@protonmail.com>
2020-10-20 00:44:38 -0700

    Add Annotation

--- app/models/user_workspace.rb
+++ app/models/user_workspace.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: user_workspaces
+#
+#  id           :bigint           not null, primary key
+#  workspace_id :bigint           not null
+#  user_id      :bigint           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class UserWorkspace < ApplicationRecord
   validates :workspace_id, :user_id, presence: true
   validates :workspace_id, uniqueness: { scope: :user_id }
