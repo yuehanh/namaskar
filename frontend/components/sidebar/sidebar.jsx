d384f8e617b8943d9b1b1574306614e20ee671e1
Yuehan <yuehan.huang@protonmail.com>
2020-10-21 03:18:52 -0700

    Create a sidebar

---
+++ frontend/components/sidebar/sidebar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ProjectIndexContainer } from '../projects/project_index_container'
+import { ProfileLinks } from '../splash/profile_links';
+
+
+export class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <div className="sidebar-header">
+          <div className="sidebar-logo">
+            <Link to='/'>
+              <img className='nav-logo' src={window.images.fullLogo} />
+            </Link>
+          </div>
+        </div>
+        <div className="sidebar-nav-link">
+          <ProfileLinks />
+          <Link to={{
+            pathname: "/home",
+          }}>
+            <div className="nav-link-home">
+              <i className="fas fa-home"></i> Home
+             </div>
+          </Link>
+          <ProjectIndexContainer />
+        </div>
+      </div >
+    )
+  }
+}
