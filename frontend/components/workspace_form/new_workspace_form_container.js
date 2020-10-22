6015eecad74be1b2ea2018cd1d6c32d2f10126d5
Yuehan <yuehan.huang@protonmail.com>
2020-10-21 17:46:34 -0700

    Add clearing erros

---
+++ frontend/components/workspace_form/new_workspace_form_container.js
@@ -0,0 +1,31 @@
+import { connect } from "react-redux";
+import { clearErrors } from "../../actions/session_actions";
+import { createWorkspace } from "../../actions/workspace_actions";
+import { closeModal } from "../../actions/modal_actions";
+
+import { WorkspaceForm } from "./workspace_form";
+
+const mapStateToProps = (state) => {
+  const workspace = { name: "", description: "" };
+  return {
+    errors: state.errors.session,
+    formType: "New Workspace",
+    workspace,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    processForm: (user) => dispatch(createWorkspace(user)),
+    clearErrors: () => dispatch(clearErrors()),
+    closeModal: () => {
+      dispatch(clearErrors());
+      dispatch(closeModal());
+    },
+  };
+};
+
+export const NewWorkpaceFormContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WorkspaceForm);
