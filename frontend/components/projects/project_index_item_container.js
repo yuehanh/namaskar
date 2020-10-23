1633b2590aea4866c3344a864f9deb6c4186a6bd
Yuehan <yuehan.huang@protonmail.com>
2020-10-22 22:37:11 -0700

    Create project index item container

---
+++ frontend/components/projects/project_index_item_container.js
@@ -0,0 +1,27 @@
+import { connect } from "react-redux";
+
+import { closeModal, openModal } from "../../actions/modal_actions";
+
+import { ProjectIndexItem } from "./project_index_item";
+
+const mapStateToProps = (_null, ownProps) => {
+  ;
+  return {
+    project: ownProps.project,
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const projectId = ownProps.project.id;
+  return {
+    createProjectModal: () => dispatch(openModal({ type: "newProject" })),
+    deleteProjectModal: () => {
+      dispatch(openModal({ type: "deleteProject", data: projectId }));
+    },
+  };
+};
+
+export const ProjectIndexItemContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProjectIndexItem);
