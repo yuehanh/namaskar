18ff6647dd25e80a57a65a43d29d23fd66562d8c
Yuehan <yuehan.huang@protonmail.com>
2020-10-23 01:38:56 -0700

    Add tasks reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,10 +3,12 @@ import { usersReducer } from "./users_reducer";
 import { workspacesReducer } from "./workspaces_reducer";
 import { userWorkspacesReducer } from "./user_workspaces_reducer";
 import { projectsReducer } from "./projects_reducer";
+import { tasksReducer } from "./tasks_reducer";
 
 export const entitiesReducer = combineReducers({
   users: usersReducer,
   workspaces: workspacesReducer,
   userWorkspaces: userWorkspacesReducer,
   projects: projectsReducer,
+  tasks: tasksReducer,
 });
---
+++ frontend/reducers/tasks_reducer.js
@@ -0,0 +1,17 @@
+import { DELETE_TASK } from "../actions/task_actions";
+import { RECEIVE_WORKSPACE } from "../actions/workspace_actions";
+
+export const tasksReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_WORKSPACE:
+      debugger
+      return action.payload.tasks;
+    case DELETE_TASK:
+      delete nextState[action.payload];
+      return nextState;
+    default:
+      return state;
+  }
+};
