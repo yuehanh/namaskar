4d7b32a86a96fd894c9b9a787b3d87e21992ba79
Yuehan <yuehan.huang@protonmail.com>
2020-10-22 22:37:46 -0700

    Add connection to delete project modal

--- frontend/components/projects/project_index_item.jsx
+++ frontend/components/projects/project_index_item.jsx
@@ -1,44 +1,56 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import { closeModal } from '../../actions/modal_actions';
+export class ProjectIndexItem extends React.Component {
 
-export const ProjectIndexItem = ({ project, createProjectModal }) => {
-  let pathname, icon, className, name, item;
-
-  if (project.new) {
-    icon = <i className="fas fa-plus"></i>
-    className = "project-create-item"
-    name = "Create Project"
-    item = (
-      <div className={className} onClick={createProjectModal} >
-        <div className="project-tile">
-          {icon}
-        </div >
-        <div className="project-name">
-          {name}
-        </div>
-      </div >)
-
-  } else {
-    pathname = `/project/${project.id}`;
-    icon = <i className="fas fa-tasks"></i>;
-    className = "project-index-item"
-    name = project.name
-
-    item = (<Link to={{ pathname, project }}  >
-      <div className={className} >
-        <div className="project-tile">
-          {icon}
-        </div >
-        <div className="project-name">
-          <div>
-            {name}
-          </div>
-          <i className="fas fa-minus"></i>
-        </div>
-      </div >
-    </Link >)
+  constructor(props) {
+    super(props)
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
-  return item;
+  handleDelete() {
+    this.props.deleteProjectModal()
+  }
+  render() {
+    let pathname, icon, className, name, item;
+
+    if (this.props.project.new) {
+      icon = <i className="fas fa-plus clickable"></i>
+      className = "project-create-item clickable"
+      name = "Create Project"
+      item = (
+        <div className={className} onClick={this.props.createProjectModal} >
+          <div className="project-tile">
+            {icon}
+          </div >
+          <div className="project-name">
+            {name}
+          </div>
+        </div >)
+
+    } else {
+      pathname = `/project/${this.props.project.id}`;
+      icon = <i className="fas fa-tasks"></i>;
+      className = "project-index-item"
+      name = this.props.project.name
+      item = (
+        <div className="project-row" >
+          <Link to={{ pathname, project: this.props.project }} >
+            <div className={className} >
+              <div className="project-tile">
+                {icon}
+              </div >
+              <div className="project-name">
+                {name}
+              </div >
+            </div>
+          </Link >
+          <i className="fas fa-minus clickable" onClick={this.handleDelete} />
+        </div >
+      )
+    }
+
+    return item;
+  }
 };
