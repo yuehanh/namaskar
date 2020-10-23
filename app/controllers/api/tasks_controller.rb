5f4a01709762aa637fe15750655ac14073a009cc
Yuehan <yuehan.huang@protonmail.com>
2020-10-23 01:05:18 -0700

    Create tasks controller

---
+++ app/controllers/api/tasks_controller.rb
@@ -0,0 +1,49 @@
+class Api::TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(task_params)
+    @task.creator_id = current_user.id
+    @task.workspace_id = current_user.homespace_id
+    if @task.save
+      @workspace = @task.workspace
+      render "api/workspaces/show", status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tasks/1
+  # PATCH/PUT /tasks/1.json
+  def update
+    if @task.update(task_params)
+      @workspace = @task.workspace
+      render "api/workspaces/show", status: :ok
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    name = @task.name
+    @task.destroy
+
+    render json: ["You have successfullly deleted '#{name}''"], status: 200
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def task_params
+    params.require(:task).permit(:name, :description, :completed, :milestone, :start_date, :end_date, :due_date, :assignee_id, :project_id)
+  end
+end
