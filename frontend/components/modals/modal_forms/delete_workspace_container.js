b605fe11c523002c865c1bcd1955e040d3d1c3ce
Yuehan <yuehan.huang@protonmail.com>
2020-10-22 22:35:59 -0700

    Create delete workspace container

---
+++ frontend/components/modals/modal_forms/delete_workspace_container.js
@@ -0,0 +1,30 @@
+import { connect } from "react-redux";
+import { closeModal } from "../../../actions/modal_actions";
+import { clearErrors } from "../../../actions/session_actions";
+import { deleteWorkspace } from "../../../actions/workspace_actions";
+
+import { DeleteForm } from "./delete_form";
+
+const mapStateToProps = (_null, ownProps) => {
+  const data = ownProps.data;
+  return {
+    data,
+    formType: "delete this workspace",
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    processForm: (id) => dispatch(deleteWorkspace(id)),
+    clearErrors: () => dispatch(clearErrors()),
+    closeModal: () => {
+      dispatch(clearErrors());
+      dispatch(closeModal());
+    },
+  };
+};
+
+export const DeleteWorkspaceContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DeleteForm);
