39203b9230271085e6ea7fb825c10266405d55a3
Yuehan <yuehan.huang@protonmail.com>
2020-10-23 05:46:21 -0700

    Add complete check to tasks

--- frontend/components/tasks/task_index.jsx
+++ frontend/components/tasks/task_index.jsx
@@ -3,6 +3,13 @@ import { TaskIndexItemContainer } from './task_index_item_container'
 
 
 export class TaskIndex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.props.createTask({ name: "New Task", project_id: this.props.projectId })
+  }
   render() {
     const taskIndexItems = this.props.tasks.map(
       (task) => (
@@ -16,6 +23,14 @@ export class TaskIndex extends React.Component {
       <div className="task-index">
         <ul className="task-index-items">
           {taskIndexItems}
+          <li
+            className="task-create-item clickable" onClick={this.handleClick}
+          >
+          <div className="create-task-name">
+              Create Task
+          </div>
+          <i className="fas fa-plus clickable"></i>
+          </li>
         </ul>
       </div>
     )
--- frontend/components/tasks/task_index_item.jsx
+++ frontend/components/tasks/task_index_item.jsx
@@ -3,8 +3,29 @@ import ContentEditable from 'react-contenteditable'
 export class TaskIndexItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = props.task
+    this.state = { task: props.task, transition: "" }
     this.handleBlur = this.handleBlur.bind(this);
+    this.handleToggleComplete = this.handleToggleComplete.bind(this)
+  }
+
+  handleToggleComplete(e) {
+    e.stopPropagation()
+    const taskNow = this.state.task
+    taskNow.completed = !taskNow.completed
+    if (taskNow.completed) {
+      this.setState({ task: taskNow })
+      setTimeout(() => {
+        this.setState({ transition: "rainbow-in" })
+      }, 250)
+
+      setTimeout(() => {
+        this.setState({ transition: "" })
+      }, 1200);
+      this.props.updateTask(this.state.task)
+    } else {
+      this.setState({ task: taskNow })
+      this.props.updateTask(this.state.task)
+    }
   }
 
 
@@ -23,12 +44,15 @@ export class TaskIndexItem extends React.Component {
 
   render() {
 
+
+    const status = this.state.task.completed ? "completed" : ""
     return (
-      <div className="task-row">
+      <div className={`task-row ${this.state.transition}`}>
+        <i className={`fas fa-check ${status}`} onClick={this.handleToggleComplete}></i>
         <ContentEditable
           onChange={this.update('name')}
           onBlur={this.handleBlur}
-          html={this.state.name} />
+          html={this.state.task.name} />
       </div>
     )
   }
