d8f28312964e3b5c661ab78ac07720ba802c8afe
Yuehan <yuehan.huang@protonmail.com>
2020-10-23 01:04:08 -0700

    Create task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
-  belongs_to :creator
+  belongs_to :creator, class_name: :User
   belongs_to :workspace
-  belongs_to :assignee
-  belongs_to :project
+  belongs_to :assignee, class_name: :User, optional: true
+  belongs_to :project, optional: true
 end
