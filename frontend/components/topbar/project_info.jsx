39d14e3de1f62f380a7888ddb36313ef411e1c27
Yuehan <yuehan.huang@protonmail.com>
2020-11-20 16:10:48 -0800

    Allow project editing

--- frontend/components/topbar/project_info.jsx
+++ frontend/components/topbar/project_info.jsx
@@ -5,12 +5,61 @@ import { Redirect } from "react-router-dom";
 export class ProjectInfo extends React.Component {
   constructor(props) {
     super(props);
+    this.state = props.info;
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (!nextProps.info) return null;
+    if (prevState.id !== nextProps.info.id) {
+      return nextProps.info;
+    }
+
+    // Return null to indicate no change to state.
+    return null;
+  }
+
+  update(field) {
+    return (e) => {
+      this.setState({
+        [field]: e.target.value,
+      });
+    };
+  }
+
+  handleBlur() {
+    const info = Object.assign({}, this.state);
+    this.props.processUpdate(info);
+  }
+
+  handleEnter() {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      this.handleBlur();
+    }
   }
 
   render() {
     return (
       <div className="workspace-header-info">
-        <h2>{this.props.info.name}</h2>
+        <h2>
+          <ContentEditable
+            onChange={this.update("name")}
+            onBlur={this.handleBlur}
+            onKeyDown={this.handleEnter}
+            html={this.state.name}
+          />
+        </h2>
+        <div className="workspace-header-description">
+          <input
+            onChange={this.update("description")}
+            onBlur={this.handleBlur}
+            value={this.state.description ?? ""}
+            className="editable-description"
+            placeholder="You might want to add a description"
+          />
+        </div>
       </div>
     );
   }
