1d5db303dfa62ff1770206189fba76683c1d7f33
Yuehan <yuehan.huang@protonmail.com>
2020-11-20 22:37:57 -0800

    Add search index

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,13 @@ class Api::UsersController < ApplicationController
   before_action :set_user, only: %i[show update]
   before_action :require_logged_in, only: %i[show update]
 
+  def index
+    if params[:search]
+      @users = User.where("email OR fullname LIKE :search_string ", substring: "%#{params[:search]}%")
+      render :index
+    end
+  end
+
   def create
     @user = User.new(user_params)
     email_index = @user.email.index("@")
