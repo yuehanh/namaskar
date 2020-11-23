c8d1a85aae642b94ddb48bc079d089174f238f84
Yuehan <yuehan.huang@protonmail.com>
2020-11-22 22:39:04 -0800

    Add text when results are not found

--- frontend/components/users/user_search_list.jsx
+++ frontend/components/users/user_search_list.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 export class UsersSearchList extends React.Component {
   render() {
-    const usersSearchItems = this.props.users.map((user) => (
-      <li className="search-item" key={user.id}>
-        {user.email}
-      </li>
-    ));
+    const usersSearchItems = this.props.users.map((user) => {
+      if (user.email) {
+        return (
+          <li
+            className="search-item"
+            key={user.id}
+            onClick={() => this.props.onClick(user)}
+          >
+            {user.email}
+          </li>
+        );
+      } else {
+        return (
+          <li className="search-item" key="empty">
+            {user}
+          </li>
+        );
+      }
+    });
     return <ul className="search-list">{usersSearchItems}</ul>;
   }
 }
